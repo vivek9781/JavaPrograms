640189c0d9f5f5d10a4a9b827fd7e8cfcd99049e
vivek9781 <vivek.kumar9781@gmail.com>
2023-11-28 13:16:03 +0530

    Starting Data structure and algorithms

---
+++ DataStructureAndAlgorithms/src/com/data/searching/SearchingAlgorithms.java
@@ -0,0 +1,28 @@
+package com.data.searching;
+
+public class SearchingAlgorithms {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	
+	public static boolean linearSearch(int nums[], int num) {
+		
+		return false;
+	}
+	
+	private static int[] searchData() {
+		
+		int data[] = {12,14,21,24,32,35,36,42,46,47,51,55,57,61,63,70,72,75,88,89,91,92,99};
+		return data;
+	}
+	
+	private static int dataToSearch() {
+		
+		int num = 14;
+		return num;
+	}
+
+}
