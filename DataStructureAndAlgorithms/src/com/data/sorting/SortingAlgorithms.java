594dcab8540fbab276b093247665602c4530a164
vivek9781 <vivek.kumar9781@gmail.com>
2023-11-28 14:38:57 +0530

    creating template

--- DataStructureAndAlgorithms/src/com/data/sorting/SortingAlgorithms.java
+++ DataStructureAndAlgorithms/src/com/data/sorting/SortingAlgorithms.java
@@ -5,13 +5,21 @@ public class SortingAlgorithms {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int nums[] = unSortedData();
+		System.out.print("Unsorted Data \n" );
+		for(int num: nums)
+		System.out.print(num + " ");
+		
+		bubbleSort(nums);
 	}
 	
 	private static void bubbleSort(int nums[]) {
+		// 7,5,3,8,9,2,6
+		System.out.println(" \n\nStarting bubble sort ...");
 		
 	}
 	
-	private int[] unSortedDate() {
+	private static int[] unSortedData() {
 		
 		int nums[] = {12,42,23,24,14,25,65,33,55,65,84};
 		return nums;
