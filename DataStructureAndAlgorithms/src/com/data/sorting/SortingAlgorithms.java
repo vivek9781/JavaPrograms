b45de15a2d16ed26095a3392bfa6f6f767c5b54e
vivek9781 <vivek.kumar9781@gmail.com>
2023-11-28 14:23:42 +0530

    starting Sorting algorithms

---
+++ DataStructureAndAlgorithms/src/com/data/sorting/SortingAlgorithms.java
@@ -0,0 +1,19 @@
+package com.data.sorting;
+
+public class SortingAlgorithms {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private static void bubbleSort(int nums[]) {
+		
+	}
+	
+	private int[] unSortedDate() {
+		
+		int nums[] = {12,42,23,24,14,25,65,33,55,65,84};
+		return nums;
+	}
+}
