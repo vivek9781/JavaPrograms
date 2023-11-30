3195fb653d2a819c82ccba9029f3c95c6cfa17d3
vivek9781 <vivek.kumar9781@gmail.com>
2023-11-30 11:54:33 +0530

    Binary Search

--- DataStructureAndAlgorithms/src/com/data/searching/SearchingAlgorithms.java
+++ DataStructureAndAlgorithms/src/com/data/searching/SearchingAlgorithms.java
@@ -4,14 +4,17 @@ public class SearchingAlgorithms {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
+		int nums[]=searchData();
+		int num=dataToSearch();
+		//boolean result = linearSearch(nums, num);
+		boolean result = binarySearch(nums, num);
+		if(result) {
+			System.out.println("Number found in data");
+		}else {
+			System.out.println("Number not found in data");
+		}
 	}
 	
-	public static boolean linearSearch(int nums[], int num) {
-		
-		return false;
-	}
 	
 	private static int[] searchData() {
 		
@@ -21,8 +24,41 @@ public class SearchingAlgorithms {
 	
 	private static int dataToSearch() {
 		
-		int num = 14;
+		int num = 99;
 		return num;
 	}
 
+	private static boolean linearSearch(int nums[], int num) {
+		
+		for(int i=0; i<nums.length;i++) {
+			if(num == nums[i]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private static boolean binarySearch(int nums[], int num) {
+		// 4,6,7,3,9,5,6
+		// 7
+		int left=0;
+		int right=nums.length-1;
+		int size=nums.length;
+		int mid = (left+right)/2;
+		
+		for(int i=0; i<size; i++) {
+			
+			if(num == nums[mid]) {
+				System.out.println("Checking equal");
+				return true;
+			}else if(num > nums[mid]) {
+				left=mid+1;
+				mid=(left+right)/2;
+			}else if(num < nums[mid]) {
+				right=mid-1;
+				mid=(left+right)/2;
+			}
+		}
+		return false;
+	}
 }
